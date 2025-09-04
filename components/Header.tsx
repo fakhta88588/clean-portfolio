bea6cea36d927d6b3ca81fa53f1955bbf14cba57
fakhta88588 <fakhtagullfakhtagull@gmail.com>
2025-09-05 04:52:54 +0500

    components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-800 text-slate-200 text-center py-6">
+      © {new Date().getFullYear()} Ocean Studio. All rights reserved.
+    </footer>
+  )
+}
