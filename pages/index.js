9a3b6fb637791181aceeb7d998ea918e8c267a6e
fakhta88588 <fakhtagullfakhtagull@gmail.com>
2025-09-05 00:36:56 +0500

    pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-indigo-500 to-pink-500 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome Next.js</h1>
+    </div>
+  );
+}
