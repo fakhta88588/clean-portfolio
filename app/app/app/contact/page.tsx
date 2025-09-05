6859ff63bb248d24a9975c398235bebb9de476d8
fakhta88588 <fakhtagullfakhtagull@gmail.com>
2025-09-05 17:06:53 +0500

    app/contact/page.tsx

---
+++ app/app/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '@/components/ContactForm'
+
+export default function ContactPage() {
+  return (
+    <section>
+      <h1>Contact</h1>
+      <ContactForm />
+    </section>
+  )
+}
